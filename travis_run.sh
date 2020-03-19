4276fa2f80291b1c198f146211caceb17b9b0184
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-03-19 19:04:23 +0100

    Update examples block (#18)
    * Update examples block
    
    * Fix shard tests
    
    * Fix shard tests
    
    * Fix tests for mongodb_replicaset
    
    * Correct module reference
    
    * Fix shebang and travis run script
    
    * Bash script fixes
    
    * Update ignores
    
    * Update ignores
    
    * Remove empty lines
    
    * Update ignores
    
    * Update ignore file
    
    * Delete INSTALL.rst files
    
    * Update examples
    
    * Update travis_run script
    
    * Increment version to 4.2
    
    * Rollback version
    
    * Yaml formatting
    
    * Fix for apt cache problem
    
    * Fix for apt cache problem
    
    * Update fix
    
    * Change apt-key method
    
    * Fix var ref
    
    * Add fix
    
    * Remove shell task
    
    * Add debug step
    
    * Update shell command
    
    * Remove shell cmd
    
    * Update travis yml
    
    * Install community.general collection
    
    * Lower comminutiy.general collection version requirement
    
    * Remove windows dependency
    
    * Add community.mongodb module path
    
    * Remove auth details
    
    * Add pause
    
    * Auth
    
    * Remove user create
    
    * Dummy commit

--- travis_run.sh
+++ travis_run.sh
@@ -9,7 +9,7 @@
 set -u;
 set -e;
 
-echo $(pwd);
+pwd;
 
 export ANSIBLE_LIBRARY="/home/travis/build/ansible-collections/mongodb/plugins/modules/";
 
@@ -34,16 +34,20 @@ for role in $(git diff --name-only HEAD~1 | grep roles/ | cut -d'/' -f -2 | sort
     fi;
 done
 
-for role in "${role_list[@]}"; do
-  echo "Executing tests for $role.";
-  cd "$role"
-  molecule test;
-  cd ../../ && echo "Back in $(pwd)"; # back to project root
-  test_count=$(( test_count + 1 ));
-done;
+if [ ${#role_list[@]} -ne 0 ]; then
+  for role in "${role_list[@]}"; do
+    echo "Executing tests for $role.";
+    cd "$role"
+    molecule test;
+    cd ../../ && echo "Back in $(pwd)"; # back to project root
+    test_count=$(( test_count + 1 ));
+  done;
 
-if (( test_count > 0 )); then
-  echo "Executed tests for a total of $test_count roles.";
+  if (( test_count > 0 )); then
+    echo "Executed tests for a total of $test_count roles.";
+  else
+    echo "No role tests executed on this run."
+  fi;
 else
-  echo "No role tests executed on this run."
+  echo "No roles in commit. Bailing without doing anything"
 fi;
