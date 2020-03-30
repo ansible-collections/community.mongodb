60d49ab0c3e597a80beedd9dadff823b258213f9
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-03-30 21:26:00 +0200

    Add role

---
+++ roles/mongodb_selinux/files/compile_mongodb_selinux.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e;
+set -u:
+
+checkmodule -M -m -o /root/mongodb_cgroup_memory.mod /root/mongodb_cgroup_memory.te
+semodule_package -o /root/mongodb_cgroup_memory.pp -m /root/mongodb_cgroup_memory.mod
+semodule -i /root/mongodb_cgroup_memory.pp
+touch /root/mongodb_selinux.success;
