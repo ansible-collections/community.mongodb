f1ae43f7ebb053f16c92f2f2a374d45e1ad3141d
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-04-04 18:42:56 +0200

    Update tests

--- roles/mongodb_mongos/molecule/virtualbox/tests/test_default.py
+++ roles/mongodb_mongos/molecule/virtualbox/tests/test_default.py
@@ -24,25 +24,42 @@ def include_vars(host):
 
 
 def test_mongod_cnf_file(host):
-    mongodb_user = include_vars(host)['ansible_facts']['mongodb_user']
-    mongodb_group = include_vars(host)['ansible_facts']['mongodb_group']
-    f = host.file('/etc/mongos.conf')
+    if host.ansible.get_variables()['inventory_hostname'] != 'config1':
+        mongodb_user = include_vars(host)['ansible_facts']['mongodb_user']
+        mongodb_group = include_vars(host)['ansible_facts']['mongodb_group']
+        f = host.file('/etc/mongos.conf')
 
-    assert f.exists
-    assert f.user == mongodb_user
-    assert f.group == mongodb_group
+        assert f.exists
+        assert f.user == mongodb_user
+        assert f.group == mongodb_group
 
 
 def test_mongod_service(host):
-    mongos_service = include_vars(host)['ansible_facts']['mongos_service']
-    s = host.service(mongos_service)
 
-    assert s.is_running
-    assert s.is_enabled
+    if host.ansible.get_variables()['inventory_hostname'] != 'config1':
+        mongos_service = include_vars(host)['ansible_facts']['mongos_service']
+        s = host.service(mongos_service)
+
+        assert s.is_running
+        assert s.is_enabled
 
 
 def test_mongod_port(host):
-    port = include_vars(host)['ansible_facts']['mongos_port']
-    s = host.socket("tcp://0.0.0.0:{0}".format(port))
+    if host.ansible.get_variables()['inventory_hostname'] != 'config1':
+        port = include_vars(host)['ansible_facts']['mongos_port']
+        s = host.socket("tcp://0.0.0.0:{0}".format(port))
 
-    assert s.is_listening
+        assert s.is_listening
+
+
+def test_mongos_shell_connectivity(host):
+    '''
+    Tests that we can connect to mongos via the shell annd run a cmd
+    '''
+    if host.ansible.get_variables()['inventory_hostname'] != 'config1':
+        port = include_vars(host)['ansible_facts']['mongos_port']
+        cmd = host.run("mongo admin --port {0} --eval 'db.runCommand({{listDatabases: 1}})'".format(port))
+
+        assert cmd.rc == 0
+        assert "config" in cmd.stdout
+        assert "admin" in cmd.stdout
