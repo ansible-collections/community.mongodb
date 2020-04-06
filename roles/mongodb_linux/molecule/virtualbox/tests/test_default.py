47aec841fe61b9c26741c6a2bd95344eaf66ec2c
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-04-06 17:11:41 +0200

    Mongodb linux (#36)
    * Init role
    
    * Complete docker tests
    
    * pep8
    
    * Delete file
    
    * Increase sleep
    
    * Update tasks
    
    * Update script
    
    * Add VB tests and update docker tests
    
    * Update ignores
    
    * Delete file
    
    * Delete dupe become

---
+++ roles/mongodb_linux/molecule/virtualbox/tests/test_default.py
@@ -0,0 +1,75 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_ntp_package(host):
+    ntp = host.package("ntp")
+    chrony = host.package("chrony")
+    assert ntp.is_installed or chrony.is_installed
+
+
+def test_ntpd_service(host):
+    ntpd = host.service("ntpd")
+
+    if ntpd.is_running:
+        assert ntpd.is_enabled
+    else:
+        ntp = host.service("ntp")
+        if ntp.is_running:
+            assert ntp.is_enabled
+        else:
+            chronyd = host.service("chronyd")
+            assert chronyd.is_running
+            assert chronyd.is_enabled
+
+
+def test_swappiness_value(host):
+    cmd = host.run("cat /proc/sys/vm/swappiness")
+
+    assert cmd.rc == 0
+    assert cmd.stdout.strip() == "1"
+
+
+def test_thp_service_file(host):
+    f = host.file("/etc/systemd/system/disable-transparent-huge-pages.service")
+
+    assert f.exists
+    assert f.user == "root"
+    assert f.group == "root"
+
+
+def test_thp_service(host):
+    '''
+    Validates the service actually works
+    '''
+    switches = ["/sys/kernel/mm/transparent_hugepage/enabled",
+                "/sys/kernel/mm/transparent_hugepage/defrag"]
+
+    is_docker = host.file("./is_docker.txt").exists
+
+    if is_docker is False:
+        for d in switches:
+            cmd = host.run("cat {0}".format(d))
+
+            assert cmd.rc == 0
+            assert "[never]" in cmd.stdout
+
+
+def test_limit_file(host):
+    f = host.file("/etc/security/limits.conf")
+
+    assert f.exists
+    assert f.user == "root"
+    assert f.group == "root"
+
+    assert f.contains("mongodb	hard	nproc	64000")
+    assert f.contains("mongodb	hard	nofile	64000")
+    assert f.contains("mongodb	soft	nproc	64000")
+    assert f.contains("mongodb	soft	nofile	64000")
+    assert f.contains("mongodb	hard	memlock	1024")
+    assert f.contains("mongodb	soft	memlock	1024")
