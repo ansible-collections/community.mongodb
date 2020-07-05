eec2fe732050935f35bf5b7364db23eb8f8e694d
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-07-05 23:08:44 +0200

    Remove line

--- plugins/cache/mongodb.py
+++ plugins/cache/mongodb.py
@@ -71,9 +71,6 @@ class CacheModule(BaseCacheModule):
             self._timeout = int(self.get_option('_timeout'))
             self._prefix = self.get_option('_prefix')
         except KeyError:
-            display.deprecated('Rather than importing CacheModules directly, '
-                               'use ansible.plugins.loader.cache_loader',
-                               collection_name="community.mongodb", version='2.10')
             self._connection = C.CACHE_PLUGIN_CONNECTION
             self._timeout = int(C.CACHE_PLUGIN_TIMEOUT)
             self._prefix = C.CACHE_PLUGIN_PREFIX
