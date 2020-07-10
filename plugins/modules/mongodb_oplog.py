e9d83f8565d2e6913e536274274a0ba4e2630bbe
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-07-10 17:10:02 +0200

    Create role stuff

--- plugins/modules/mongodb_oplog.py
+++ plugins/modules/mongodb_oplog.py
@@ -52,7 +52,7 @@ options:
     description:
       - Runs compact against the oplog.rs collection in the local database to reclaim disk space.
       - Will not run against PRIMARY members.
-      - The MongoDB user must have the readWrite role on the local database for this feature to work.
+      - The MongoDB user must have the compact role on the local database for this feature to work.
     type: bool
     default: false
     required: false
