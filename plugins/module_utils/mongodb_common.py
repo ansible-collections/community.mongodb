9919706d1fc75d21abcb94257319dcf383a63cfd
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-08-05 15:44:02 +0200

    Change imports

--- plugins/module_utils/mongodb_common.py
+++ plugins/module_utils/mongodb_common.py
@@ -45,10 +45,10 @@ def autoinstall_pymongo(module):
         module.warn("Attempting installation of pymongo via pip.")
         module.run_command(['/usr/bin/pip3', 'install', 'pymongo'], check_rc=True)
         module.warn("Successfully installed pymongo.")
-        global_imports("pymongo.errors.ConnectionFailure","ConnectionFailure",True)
-        global_imports("pymongo.errors.OperationFailure","OperationFailure",True)
-        global_imports("pymongo.version","PyMongoVersion",True)
-        global_imports("pymongo.MongoClient","MongoClient",True)
+        globals["ConnectionFailure"] = __import__("pymongo.errors.ConnectionFailure")
+        globals["OperationFailure"] = __import__("pymongo.errors.OperationFailure")
+        globals["PyMongoVersion"] = __import__("pymongo.version")
+        globals["MongoClient"] = __import__("pymongo.MongoClient")
         pymongo_found = True
     except ImportError as excep:
         module.fail_json(msg="Could not import python pymongo: {0}".format(excep))
