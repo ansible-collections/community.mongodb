9865191d5ff5bee66ee2979932432e5921545bc3
Rhys Campbell <rhys.james.campbell@googlemail.com>
2020-10-18 17:58:49 +0200

    Update stale tests

--- roles/mongodb_mongod/molecule/virtualbox/tests/test_default.py
+++ roles/mongodb_mongod/molecule/virtualbox/tests/test_default.py
@@ -42,7 +42,10 @@ def test_mongod_service(host):
 
 
 def test_mongod_port(host):
-    port = include_vars(host)['ansible_facts']['mongod_port']
+    try:
+        port = include_vars(host)['ansible_facts']['mongod_port']
+    except KeyError:
+        port = 27017
     s = host.socket("tcp://0.0.0.0:{0}".format(port))
 
     assert s.is_listening
@@ -52,15 +55,18 @@ def test_mongod_replicaset(host):
     '''
     Ensure that the MongoDB replicaset has been created successfully
     '''
-    port = include_vars(host)['ansible_facts']['mongod_port']
+    try:
+        port = include_vars(host)['ansible_facts']['mongod_port']
+    except KeyError:
+        port = 27017
     cmd = "mongo --port {0} --eval 'rs.status()'".format(port)
     # We only want to run this once
     if host.ansible.get_variables()['inventory_hostname'] == "ubuntu_16":
         r = host.run(cmd)
 
         assert "rs0" in r.stdout
-        assert "ubuntu-16:27017" in r.stdout
-        assert "ubuntu-18:27017" in r.stdout
-        assert "debian-stretch:27017" in r.stdout
-        assert "debian-buster:27017" in r.stdout
-        assert "centos-7:27017" in r.stdout
+        assert "ubuntu-16:{0}".format(port) in r.stdout
+        assert "ubuntu-18:{0}".format(port) in r.stdout
+        assert "debian-stretch:{0}".format(port) in r.stdout
+        assert "debian-buster:{0}".format(port) in r.stdout
+        assert "centos-7:{0}".format(port) in r.stdout
