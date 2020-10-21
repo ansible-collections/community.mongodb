6d7defbcc410f2f44d6491d6140d84b74270be42
Aditya Mohan <adimohan93@gmail.com>
2020-10-21 11:22:13 +0530

    Reverting some changes back due to testinfra issues
    https://github.com/ansible-collections/community.mongodb/pull/219#discussion_r508791912

--- roles/mongodb_linux/molecule/virtualbox/tests/test_default.py
+++ roles/mongodb_linux/molecule/virtualbox/tests/test_default.py
@@ -50,7 +50,7 @@ def test_thp_service(host):
     switches = ["/sys/kernel/mm/transparent_hugepage/enabled",
                 "/sys/kernel/mm/transparent_hugepage/defrag"]
 
-    if host.ansible.get_variables()["ansible_facts"]["virtualization_type"] != 'docker':
+    if host.ansible("setup")["ansible_facts"]["ansible_virtualization_type"] != 'docker':
         for d in switches:
             cmd = host.run("cat {0}".format(d))
 
