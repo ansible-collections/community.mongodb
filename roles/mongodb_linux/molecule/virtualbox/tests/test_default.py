cb3ec9bd9ab6acb2c28d9e3139b6f97bf6b86a0b
Aditya Mohan <adimohan93@gmail.com>
2020-10-22 09:56:40 +0530

    Replace is_docker file with ansible_facts (#219)
    * Removed is_docker stuff across roles
    
    removed is_docker stuff across roles. Also fixed related tests.

--- roles/mongodb_linux/molecule/virtualbox/tests/test_default.py
+++ roles/mongodb_linux/molecule/virtualbox/tests/test_default.py
@@ -50,9 +50,7 @@ def test_thp_service(host):
     switches = ["/sys/kernel/mm/transparent_hugepage/enabled",
                 "/sys/kernel/mm/transparent_hugepage/defrag"]
 
-    is_docker = host.file("./is_docker.txt").exists
-
-    if is_docker is False:
+    if host.ansible("setup")["ansible_facts"]["ansible_virtualization_type"] != 'docker':
         for d in switches:
             cmd = host.run("cat {0}".format(d))
 
