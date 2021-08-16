e3c7b0fb7f0c41a5db8d8bc3a6f618b154b1db8e
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-08-16 21:44:10 +0200

    Add tests and a few small corrections

--- roles/mongodb_install/files/lock_mongodb_packages.sh
+++ roles/mongodb_install/files/lock_mongodb_packages.sh
@@ -7,17 +7,17 @@ set -u;
 HOLD="$1";
 PACKAGE_NAME="mongodb-org*"
 
-if [[ "$HOLD" == "HOLD" ]], then
+if [[ "$HOLD" == "HOLD" ]]; then
   if command -v yum &> /dev/null; then
     yum versionlock "$PACKAGE_NAME" && touch /root/mongo_version_lock.success;
-  elif command -v dpkg  &> /dev/null; then
-    echo "$PACKAGE_NAME hold" | sudo dpkg --set-selections && touch /root/mongo_version_lock.success;
+  elif command -v apt-mark  &> /dev/null; then
+    apt-mark hold "$PACKAGE_NAME" && touch /root/mongo_version_lock.success;
   fi;
-elif [[ "$HOLD" == "NOHOLD" ]], then
+elif [[ "$HOLD" == "NOHOLD" ]]; then
   if command -v yum &> /dev/null; then
-    yum versionlock delete "$PACKAGE_NAME" && rm -rf /root/mongo_version_lock.success;
-  elif command -v dpkg  &> /dev/null; then
-    echo "$PACKAGE_NAME install" | sudo dpkg --set-selections && rm -rf /root/mongo_version_lock.success;
+    yum versionlock delete "$PACKAGE_NAME" || true && rm -rf /root/mongo_version_lock.success;
+  elif command -v apt-mark  &> /dev/null; then
+    apt-mark unhold "$PACKAGE_NAME" && rm -rf /root/mongo_version_lock.success;
   fi;
 else
   echo "No action taken";
