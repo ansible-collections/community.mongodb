320b8742b145c0ac357ce64cf718907a5bae1e87
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-09-13 20:19:28 +0200

    Update test

--- tests/unit/test_module_utils.py
+++ tests/unit/test_module_utils.py
@@ -282,7 +282,8 @@ class TestMongoDBCommonMethods(unittest.TestCase):
         fake_module.params["login_user"] = None
         fake_module.params["login_password"] = None
         client = mongodb_common.mongo_auth(fake_module, client)
-        assert 'No credentials to authenticate' in fake_module.get_msg()
+        msg = fake_module.get_msg()
+        assert 'No credentials to authenticate' in msg
 
 if __name__ == '__main__':
     unittest.main()
