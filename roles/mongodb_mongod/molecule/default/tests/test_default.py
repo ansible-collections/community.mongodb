54a2f7521fe22389c235d67f9dd27a9f192c0bf1
Rhys Campbell <rhys.james.campbell@googlemail.com>
2021-10-13 09:03:21 +0200

    Add additional test to existing default scenarios

--- roles/mongodb_mongod/molecule/default/tests/test_default.py
+++ roles/mongodb_mongod/molecule/default/tests/test_default.py
@@ -69,3 +69,23 @@ def test_mongod_replicaset(host):
         assert "ubuntu_18:{0}".format(port) in r.stdout
         assert "debian_buster:{0}".format(port) in r.stdout
         # assert "debian_stretch:{0}".format(port) in r.stdout
+
+
+def test_mongod_default_path(host):
+    '''
+    Ensure that the default paths for RedHat and Debian based OSes are respected
+    '''
+    hostname = host.ansible.get_variables()['inventory_hostname']
+    default_path = "/var/lib/mongo"
+    if hostname.startswith('centos'):
+        default_path = "/var/lib/mongo"
+    elif hostname.startswith('ubuntu') or hostname.startswith('debian'):
+        default_path = "/var/lib/mongodb"
+
+    # assert path exists
+    f = host.file(default_path)
+    assert f.exists
+    assert f.is_directory
+    # asset mongodb.cnf contains path
+    conf = host.file('/etc/mongod.conf').content_string
+    assert "dbPath: {0}".format(default_path) in conf
