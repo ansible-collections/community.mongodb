c19fde32d666989512a8725a6d4ed8f8e75177e7
Rhys Campbell <rhys.james.campbell@googlemail.com>
2022-01-19 20:21:03 +0100

    Debug

--- tests/unit/test_module_utils.py
+++ tests/unit/test_module_utils.py
@@ -280,6 +280,7 @@ class TestMongoDBCommonMethods(unittest.TestCase):
         fake_module.params["replica_set"] = None
         mongodb_common.mongo_auth(fake_module, client)
         msg = fake_module.get_msg()
+        print(msg)
         print(fake_module.params)
         assert "When supplying login arguments" in msg
 
