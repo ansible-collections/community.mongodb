d3afd70dc47c1559e1c7bd68bcdde3fd1beaa59a
Rhys Campbell <rhys.james.campbell@googlemail.com>
2022-01-31 17:06:46 +0100

    Various

--- roles/mongodb_mongod/molecule/custom_db_path/tests/test_default.py
+++ roles/mongodb_mongod/molecule/custom_db_path/tests/test_default.py
@@ -64,11 +64,10 @@ def test_mongod_replicaset(host):
         r = host.run(cmd)
         assert "rs0" in r.stdout
         assert "centos_7:{0}".format(port) in r.stdout
-        assert "centos_8:{0}".format(port) in r.stdout
         assert "ubuntu_16:{0}".format(port) in r.stdout
         assert "ubuntu_18:{0}".format(port) in r.stdout
         assert "debian_buster:{0}".format(port) in r.stdout
-        # assert "debian_stretch:{0}".format(port) in r.stdout
+        assert "debian_stretch:{0}".format(port) in r.stdout
 
 
 def test_mongod_config_custom_path(host):
