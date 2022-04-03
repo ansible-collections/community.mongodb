4fcd310aca8ef8c51f92a39356d108320457ceb0
Rhys Campbell <rhys.james.campbell@googlemail.com>
2022-04-03 16:50:23 +0200

    Really fix yaml

--- plugins/cache/mongodb.py
+++ plugins/cache/mongodb.py
@@ -22,25 +22,25 @@ options:
         - MongoDB Connection String URI
         required: False
         env:
-        - name: ANSIBLE_CACHE_PLUGIN_CONNECTION
+          - name: ANSIBLE_CACHE_PLUGIN_CONNECTION
         ini:
-        - key: fact_caching_connection
+          - key: fact_caching_connection
             section: defaults
     _prefix:
         description: User defined prefix to use when creating the DB entries
         default: ansible_facts
         env:
-        - name: ANSIBLE_CACHE_PLUGIN_PREFIX
+          - name: ANSIBLE_CACHE_PLUGIN_PREFIX
         ini:
-        - key: fact_caching_prefix
+          - key: fact_caching_prefix
             section: defaults
     _timeout:
         default: 86400
         description: Expiration timeout in seconds for the cache plugin data. Set to 0 to never expire
         env:
-        - name: ANSIBLE_CACHE_PLUGIN_TIMEOUT
+          - name: ANSIBLE_CACHE_PLUGIN_TIMEOUT
         ini:
-        - key: fact_caching_timeout
+          - key: fact_caching_timeout
             section: defaults
         type: integer
 '''
