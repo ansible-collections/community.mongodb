5509dc61d07c2a146dac72a99f2308b6034b0490
Rhys Campbell <rhys.james.campbell@googlemail.com>
2022-05-08 10:12:29 +0200

    Update readme and small tidy up

--- plugins/module_utils/mongodb_common.py
+++ plugins/module_utils/mongodb_common.py
@@ -39,47 +39,6 @@ def check_compatibility(module, srv_version, driver_version):
             module.warn("You should use pymongo 3.12+ or 4+ but {0} was found.".format(driver_version))
 
 
-# def _check_compatibility(module, srv_version, driver_version):
-#     """Check the compatibility between the driver and the database.
-
-#     See: https://docs.mongodb.com/ecosystem/drivers/driver-compatibility-reference/#python-driver-compatibility
-
-#     Args:
-#         module: Ansible module.
-#         srv_version (LooseVersion): MongoDB server version.
-#         driver_version (LooseVersion): Pymongo version.
-#     """
-#     msg = 'pymongo driver version and MongoDB version are incompatible: '
-
-#     if srv_version >= LooseVersion('5.0') and driver_version < LooseVersion('3.12'):
-#         msg += 'you must use pymongo 3.12+ with MongoDB >= 5.0'
-#         module.fail_json(msg=msg)
-#     elif srv_version >= LooseVersion('4.4') and driver_version < LooseVersion('3.11'):
-#         msg += 'you must use pymongo 3.11+ with MongoDB >= 4.4'
-#         module.fail_json(msg=msg)
-#     elif srv_version >= LooseVersion('4.2') and driver_version < LooseVersion('3.9'):
-#         msg += 'you must use pymongo 3.9+ with MongoDB >= 4.2'
-#         module.fail_json(msg=msg)
-#     elif srv_version >= LooseVersion('4.0') and driver_version < LooseVersion('3.7'):
-#         msg += 'you must use pymongo 3.7+ with MongoDB >= 4.0'
-#         module.fail_json(msg=msg)
-#     elif srv_version >= LooseVersion('3.6') and driver_version < LooseVersion('3.6'):
-#         msg += 'you must use pymongo 3.6+ with MongoDB >= 3.6'
-#         module.fail_json(msg=msg)
-#     elif srv_version >= LooseVersion('3.4') and driver_version < LooseVersion('3.4'):
-#         msg += 'you must use pymongo 3.4+ with MongoDB >= 3.4'
-#         module.fail_json(msg=msg)
-#     elif srv_version >= LooseVersion('3.2') and driver_version < LooseVersion('3.2'):
-#         msg += 'you must use pymongo 3.2+ with MongoDB >= 3.2'
-#         module.fail_json(msg=msg)
-#     elif srv_version >= LooseVersion('3.0') and driver_version <= LooseVersion('2.8'):
-#         msg += 'you must use pymongo 2.8+ with MongoDB 3.0'
-#         module.fail_json(msg=msg)
-#     elif srv_version >= LooseVersion('2.6') and driver_version <= LooseVersion('2.7'):
-#         msg += 'you must use pymongo 2.7+ with MongoDB 2.6'
-#         module.fail_json(msg=msg)
-
-
 def load_mongocnf():
     config = configparser.RawConfigParser()
     mongocnf = os.path.expanduser('~/.mongodb.cnf')
@@ -340,7 +299,7 @@ def mongo_auth(module, client, directConnection=False):
     login_password = module.params['login_password']
     login_database = module.params['login_database']
 
-    fail_msg = None  # Out test code had issues with multiple exist points wiht fail_json
+    fail_msg = None  # Our test code had issues with multiple exit points with fail_json
 
     crypt_flag = 'ssl'
     if 'tls' in module.params:
