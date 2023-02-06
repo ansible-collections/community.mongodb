c32e21e5100404ac9fae714280bd88b18c6f7a82
Rhys Campbell <rhys.james.campbell@googlemail.com>
2023-02-06 10:20:27 +0100

    Update tests to use mongosh

--- roles/mongodb_config/molecule/custom_db_path/tests/test_default.py
+++ roles/mongodb_config/molecule/custom_db_path/tests/test_default.py
@@ -52,7 +52,7 @@ def test_mongod_replicaset(host):
     Ensure that the MongoDB config replicaset has been created successfully
     '''
     port = include_vars(host)['ansible_facts']['config_port']
-    cmd = "mongo --port {0} --eval 'rs.status()'".format(port)
+    cmd = "mongosh --port {0} --eval 'rs.status()'".format(port)
     # We only want to run this once
     if host.ansible.get_variables()['inventory_hostname'] == "amazonlinux":
         r = host.run(cmd)
