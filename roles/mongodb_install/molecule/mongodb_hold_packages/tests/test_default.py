80af7699dc153253c1b92cdbfab4529ee43ff941
Rhys Campbell <rhys.james.campbell@googlemail.com>
2023-09-15 14:57:53 +0200

    Update tests

--- roles/mongodb_install/molecule/mongodb_hold_packages/tests/test_default.py
+++ roles/mongodb_install/molecule/mongodb_hold_packages/tests/test_default.py
@@ -19,8 +19,6 @@ def test_mongodb_packages_not_installed(host):
     assert p.is_installed
     p = host.package("mongodb-mongosh")
     assert p.is_installed
-    p = host.package("mongodb-org-mongos")
-    assert p.is_installed
     p = host.package("mongodb-org-tools")
     assert p.is_installed
 
