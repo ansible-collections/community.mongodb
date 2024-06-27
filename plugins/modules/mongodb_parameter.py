bac05327d618d6fb76ac88789d5fbb36d1fc98d9
Gert van den Berg <gert.vandenberg@iotnxt.com>
2024-06-27 09:32:22 +0200

    Update documentation to remove broken links and refer to the correct minimum pymongo version

--- plugins/modules/mongodb_parameter.py
+++ plugins/modules/mongodb_parameter.py
@@ -46,9 +46,9 @@ options:
         choices: [int, str]
 
 notes:
-    - Requires the pymongo Python package on the remote host, version 2.4.2+.
+    - Requires the pymongo Python package on the remote host, version 4+.
     - This can be installed using pip or the OS package manager.
-    - See also U(http://api.mongodb.org/python/current/installation.html)
+    - See also U(https://www.mongodb.com/docs/languages/python/pymongo-driver/current/get-started/download-and-install/)
 requirements: [ "pymongo" ]
 author: "Loic Blot (@nerzhul)"
 '''
--- plugins/modules/mongodb_user.py
+++ plugins/modules/mongodb_user.py
@@ -79,9 +79,9 @@ options:
       - If this file is present (and C(login_user) is not defined), then skip this task.
 
 notes:
-    - Requires the pymongo Python package on the remote host, version 2.4.2+. This
+    - Requires the pymongo Python package on the remote host, version 4+. This
       can be installed using pip or the OS package manager. Newer mongo server versions require newer
-      pymongo versions. @see http://api.mongodb.org/python/current/installation.html
+      pymongo versions. @see https://www.mongodb.com/docs/languages/python/pymongo-driver/current/compatibility/
 requirements:
   - "pymongo"
 author:
