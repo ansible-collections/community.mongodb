da854cec8a82b5753c02ba48eb22a69b7f524bdf
Rhys Campbell <rhys.james.campbell@googlemail.com>
2026-04-11 12:50:25 +0200

    Remove debug cmd that breaks unit tests

--- plugins/modules/mongodb_user.py
+++ plugins/modules/mongodb_user.py
@@ -279,7 +279,6 @@ def user_add(module, client, db_name, user, password, roles, authentication_rest
 
     try:
         exists = user_find(client, user, db_name)
-        module.warn(f"user find returned {str(exists)}")
     except Exception as excep:
         # We get this exception: "not authorized on admin to execute command"
         # when auth is enabled on a new instance. The loalhost exception should
